963a348c3a839aa7bee324fdecbc66c4117ff3cd
HarishKumarS <harishkumarsedu@gmail.com>
2024-05-08 14:59:39 +0200

    data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.housing import log
+import os 
+import opendatasets as od 
 
-log.info('hi')
+import urllib.request as request 
+
+def download_file():
+    od.download(
+            dataset_id_or_url = 'https://www.kaggle.com/amitabhajoy/bengaluru-house-price-data',
+        )
+
+download_file()
