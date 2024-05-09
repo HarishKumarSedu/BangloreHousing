590cd68f0686afaf8bdcce8ea7f507944d737d1e
HarishKumarS <harishkumarsedu@gmail.com>
2024-05-09 13:07:02 +0200

    Model Trainer Template added

--- src/housing/components/model_trainer.py
+++ src/housing/components/model_trainer.py
@@ -1,4 +1,18 @@
+
+import os 
+import json
+import pandas as pd
+import numpy as np 
+from sklearn.model_selection import train_test_split
+from src.housing.constants import CONFIG_FILE_PATH, SCHEMA_FILE_PATH
+from src.housing.utils.common import read_yaml
+from src.housing import log
+from sklearn.linear_model import LinearRegression
+
 class ModelTrainer:
-  
-  def __init__(self):
-    pass
+
+    def __init__(self) -> None:
+        self.config = read_yaml(CONFIG_FILE_PATH)
+        self.data_ingestion = self.config.data_ingestion
+        self.data_validation = self.config.data_validation
+        self.data_transformation = self.config.data_transformation
